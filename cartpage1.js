b3c23e84849debb2885f49195be68f108788f41d
avyr12 <103566427+avyr12@users.noreply.github.com>
2022-05-22 22:26:44 +0530

    commit 22-05-2022

---
+++ cartpage1.js
@@ -0,0 +1,6 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("multiply").cartpage1HTML = num1 * num2;
+}
